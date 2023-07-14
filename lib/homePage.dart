0ac4d3448e17d2cc70068a9d660fe62ca8741cec
Hiu Lam Lau <hlau0019@student.monash.edu>
2023-07-14 19:08:10 +1000

    homepage UI update

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -5,15 +5,26 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            'Welcome back',
-            style: Theme.of(context).textTheme.headlineLarge,
-          ),
-        ],
+    return Scaffold(
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.fromLTRB(16, 45, 0, 0),
+              height: MediaQuery.of(context).size.height*0.4,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(96, 41, 233, 201)
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Good Plan")
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
